61fb2584c904ace6d3897a66e04d2f422d9fbbc3
Matteo Gildone <matteo.gildone@gmail.com>
2013-08-27 17:39:11 +0100

    first commit

--- wp_widget.js
+++ wp_widget.js
@@ -1,4 +1,5 @@
 /* global $*/
+var delegateEventSplitter = /^(\S+)\s*(.*)$/;
 
 var Wp_Widget = function (options) {
     this.initialize.apply(this, arguments);
@@ -10,7 +11,26 @@ $.extend(Wp_Widget.prototype, {
         console.log(arguments);
     },
     delegateEvents : function (events) {
+        events = this.events
+//        if (!events || !(events = this.events)) {
+//            return this;
+//        }
+        for (var key in events) {
+            var method = events[key];
+            var match = key.match(delegateEventSplitter);
+            var eventName = match[1], selector = match[2];
+            method = $.proxy(method, this);
+            eventName += '.delegateEvents' + this.cid;
+            if (selector === '') {
+                this.$el.on(eventName, method);
+            } else {
+                this.$el.on(eventName, selector, method);
+            }
+        }
+        console.log(this.events);
         console.log('EVENTS fire!');
+
+        return this;
     }
 });
 
